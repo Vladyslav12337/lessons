7d97f856f7fac3a234d7db8750f775e789df70c8
Vladyslav12337 <144814892+Vladyslav12337@users.noreply.github.com>
2024-02-11 22:28:12 +0100

    Add files via upload

--- hydravr/js/script.js
+++ hydravr/js/script.js
@@ -23,15 +23,15 @@ icon.addEventListener('click', function () {
 
 
 let _slideUp = (target, duration = 500) => {
-  // your slideUp function
+	// your slideUp function
 };
 
 let _slideDown = (target, duration = 500) => {
-  // your slideDown function
+	// your slideDown function
 };
 
 let _slideToggle = (target, duration = 500) => {
-  // your slideToggle function
+	// your slideToggle function
 };
 
 
@@ -40,32 +40,127 @@ let _slideToggle = (target, duration = 500) => {
 const choiseSlider = document.querySelector('.info');
 
 if (choiseSlider) {
-  const swiper = new Swiper('.info', {
-    // Optional parameters
-    loop: true,
-    autoHeight: true,
-    slidesPerView: 1,
+	const swiper = new Swiper('.info', {
+		// Optional parameters
+		loop: true,
+		autoHeight: true,
+		slidesPerView: 1,
 
-    // Breakpoints
-    breakpoints: {
-      // При ширине экрана больше 580px
-      580: {
-        slidesPerView: 2,
-      },
-      // При ширине экрана меньше 580px
-    },
+		// Breakpoints
+		breakpoints: {
+			// При ширине экрана больше 580px
+			580: {
+				slidesPerView: 2,
+			},
+			// При ширине экрана меньше 580px
+		},
 
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
+		// If we need pagination
+		pagination: {
+			el: '.swiper-pagination',
+		},
 
-    // Navigation arrows
-    navigation: {
-      nextEl: '.info__arrow--next',
-      prevEl: '.info__arrow--prev',
-    },
-  });
+		// Navigation arrows
+		navigation: {
+			nextEl: '.info__arrow--next',
+			prevEl: '.info__arrow--prev',
+		},
+	});
+}
+
+
+const categorySlider = document.querySelector('.choise');
+
+if (categorySlider) {
+	const swiper = new Swiper('.choise', {
+		// Optional parameters
+		loop: true,
+		autoHeight: true,
+		slidesPerView: 1,
+		spaceBetween: 11,
+
+		breakpoints: {
+			// При ширине экрана больше 580px
+			550: {
+				slidesPerView: 2,
+			},
+			// При ширине экрана меньше 580px
+			750: {
+				slidesPerView: 3,
+			},
+			881: {
+				slidesPerView: 4,
+			}
+		},
+
+		// Navigation arrows
+		navigation: {
+			nextEl: '.choise__arrow--next',
+			prevEl: '.choise__arrow--prev',
+		},
+	});
+}
+
+const variantSlider = document.querySelector('.variant');
+
+if (variantSlider) {
+	const swiper = new Swiper('.variant', {
+		// Optional parameters
+		loop: true,
+		autoHeight: true,
+		slidesPerView: 1,
+		spaceBetween: 11,
+
+		breakpoints: {
+			// При ширине экрана больше 580px
+			550: {
+				slidesPerView: 2,
+			},
+			// При ширине экрана меньше 580px
+			750: {
+				slidesPerView: 3,
+			},
+			881: {
+				slidesPerView: 4,
+			}
+		},
+
+		// Navigation arrows
+		navigation: {
+			nextEl: '.variant__arrow--next',
+			prevEl: '.variant__arrow--prev',
+		},
+	});
+}
+
+const buildSlider = document.querySelector('.build');
+
+if (variantSlider) {
+	const swiper = new Swiper('.build', {
+		// Optional parameters
+		loop: true,
+		autoHeight: true,
+		slidesPerView: 1,
+		spaceBetween: 20,
+
+		breakpoints: {
+			// При ширине экрана больше 580px
+			375: {
+				slidesPerView: 2,
+			},
+			// При ширине экрана меньше 580px
+			750: {
+				slidesPerView: 3,
+			},
+
+		},
+
+		// Navigation arrows
+		navigation: {
+			nextEl: '.build__arrow--next',
+			prevEl: '.build__arrow--prev',
+		},
+	});
 }
 
 
